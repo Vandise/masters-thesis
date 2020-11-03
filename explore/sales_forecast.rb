207900fae3251400e4c301d57626a5ccc43fef3f
Benjamin Anderson <andeb2804@gmail.com>
2020-11-02 20:30:33 -0600

    begin sales forecast

--- explore/sales_forecast.rb
+++ explore/sales_forecast.rb
@@ -3,41 +3,44 @@ require 'enumerator'
 
 srand(12)
 
-FREE_THRESHOLD = 0.60
-PERSONAL_THRESHOLD = 0.25
-PROFESSIONAL_THRESHOLD = 0.10
-ENTERPRISE_THRESHOLD = 0.05
+#CPM
+LN_CPM = 20000 # 131.8, 0.39 click rate, 6.59/1k impressions
 
-thresholds = [FREE_THRESHOLD, PERSONAL_THRESHOLD, PROFESSIONAL_THRESHOLD, ENTERPRISE_THRESHOLD]
+# 2%
+GG_CPM = 42000 # 118 , 2% click rate, 2.80/1000 impressions
+
+VISITORS = (LN_CPM*0.0039 + GG_CPM*0.02)
+CONVERSION_RATE = 0.01
+CUSTOMERS = (VISITORS * CONVERSION_RATE).floor()
+
+FREE_THRESHOLD = 0.85
+PERSONAL_THRESHOLD = 0.95
+PROFESSIONAL_THRESHOLD = 0.98
+ENTERPRISE_THRESHOLD = 0.98
+
+thresholds = [
+  Proc.new { |t| t <= FREE_THRESHOLD },
+  Proc.new { |t| t > FREE_THRESHOLD && t <= PERSONAL_THRESHOLD},
+  Proc.new { |t| t > PROFESSIONAL_THRESHOLD && t <= ENTERPRISE_THRESHOLD},
+  Proc.new { |t| t > ENTERPRISE_THRESHOLD} 
+]
+
 subscriptions = []
 
-def free_tier_subs
-  rand(0...10)
-end
-
-def personal_tier_subs
-  rand(0...5)
-end
-
-def professional_tier_subs
-  rand(0...3)
-end
-
-def enterprise_tier_subs
-  rand(0...2)
-end
-
 12.times.each do |m|
-  threshold = rand()
+  sub_model_amts = [0,0,0,0]
 
-  subscribe_thresholds = [free_tier_subs, personal_tier_subs, professional_tier_subs, enterprise_tier_subs]
-
-  subscribe_thresholds.each_with_index do |month_subs, index|
-    if threshold <= thresholds[index]
-      subscriptions << month_subs
-    else
-      subscriptions << 0
+  (1...CUSTOMERS).each do
+    threshold_rand = rand()
+    thresholds.each_with_index do |threshold_proc, index|
+      if threshold_proc.call(threshold_rand)
+        sub_model_amts[index] += 1
+      end
     end
+  end
+
+  sub_model_amts.each do |subs|
+    subscriptions << subs
   end
 end
 
@@ -45,7 +48,7 @@ tiers = ['free','personal','professional','enterprise']
 tier_revenues = [-0.10, 36.94, 88.68, 1243.00]
 
 CSV.open('./explore/forecast.csv', 'wb') do |csv|
-  csv << ['subscription', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'revenue']
+  csv << ['subscription', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'total revenue']
 
   tiers.each_with_index do |tier, index|
     data = []
